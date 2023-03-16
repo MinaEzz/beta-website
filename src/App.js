33f2f6556c9a1ad1d4d326562867292ed0fcd48c
MinaEzz <minaezzat98@gmail.com>
2023-03-16 21:38:49 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Workshop from "./components/Workshop.jsx";
 import Footer from "./components/Footer.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -13,10 +18,11 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/workshop" element={<Workshop />}></Route>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
         </Routes>
         <Footer />
       </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import '../css/Navbar.css';
 
 function Nav_bar() {
@@ -9,21 +9,21 @@ return (
     <Navbar expand="lg" className='fixed-top'>
         <div className='container'>
             <Navbar.Brand className='logo'> 
-            <Link to={'/'}>
+            <NavLink to={'/home'}>
             <img src="Assets/images/logo.png" alt='logo' draggable='false'></img>
-            </Link>
+            </NavLink>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
             <Nav.Link>
-                <Link to={'/'} className="nav-link nav-link-hov active ">Home</Link>
+                <NavLink to={'/home'} className="nav-link nav-link-hov  ">Home</NavLink>
                 </Nav.Link> 
                 <Nav.Link>
-                <Link to={'/about'} className="nav-link nav-link-hov ">About</Link>
+                <NavLink to={'/about'} className="nav-link nav-link-hov ">About</NavLink>
                 </Nav.Link>
                 <Nav.Link>
-                <Link to={'/contact'} className="nav-link nav-link-hov ">Contact Us</Link>
+                <NavLink to={'/contact'} className="nav-link nav-link-hov ">Contact Us</NavLink>
                 </Nav.Link>
             </Nav>
             </Navbar.Collapse>
